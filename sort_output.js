195b806ffcdaf3e91bef182238201698cfb5f8d6
imfulee <freems5192@gmail.com>
2020-07-03 03:21:22 +0800

    Just formating

--- sort_output.js
+++ sort_output.js
@@ -1,3 +1,5 @@
 function sortingByStudentId() {
-  SpreadsheetApp.openById('1LWvBu3oRkT1VrKmR78_0vQ1T8bKOFitHn5JIlKHPn9k').sort(3).sort(4);
+  SpreadsheetApp.openById("1LWvBu3oRkT1VrKmR78_0vQ1T8bKOFitHn5JIlKHPn9k")
+    .sort(3)
+    .sort(4);
 }
